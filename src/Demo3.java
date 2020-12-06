4080dc3eb8432d420c83e8cf79a42b6e1bc232a0
Sumedh <lets.code@yahoo.com>
2020-12-06 11:20:29 +0530

    added two file

---
+++ src/Demo3.java
@@ -0,0 +1,16 @@
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int a = 30;
+		int b = 40;
+		
+		int c = a+b;
+		
+		System.out.println(c);
+	}
+
+}
